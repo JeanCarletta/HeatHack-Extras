dc74673b70fa8f79d7c177729987bd19ac66df22
Jean Carletta <jeanc939@gmail.com>
2025-11-08 09:30:21 +0000

    new venue 86 data but there appears to be some doubled

--- aux/clean_up_thermopro.py
+++ aux/clean_up_thermopro.py
@@ -2,14 +2,16 @@
 #  Run locally before downsampling, although we aren't sure the cleanup technique is universal!!
 #  * Gets rid of non-ascii characters
 #  * Concatenates date and time fields
+# 
 #  * TODO - get rid of any lines with a temp or rh of -.  These really mess up the plotting.
+# * TODO - Integrate this with the main plotting.
 #  Sometimes we  also need to fix 12 vs 24 hr clock, but that takes judgment (although it's spottable that venues are colder at night...)
 
 
 # EDIT THESE VARIABLES BEFORE RUNNING
 
-csv_filepath = "../venue-AS/thermopro/raw"
-csv_outpath = "../venue-AS/thermopro/clean"
+csv_filepath = "../venue-86/tranche2/raw"
+csv_outpath = "../venue-86/tranche2/clean"
 number_of_header_lines = 2
 
 
@@ -25,6 +27,7 @@ for f in csv_files:
     df['date'] = df['date'].apply(lambda x: ''.join(char for char in x if ord(char) < 128))
     df['time'] = df['time'].apply(lambda x: ''.join(char for char in x if ord(char) < 128))
     df['time'] = df['date'] + ' ' + df['time']
+    df['time'] = df['time'].apply(lambda x: x.replace('/','-')) # seems that now Thermopros output eg 15/12/2024 and we need to either read that or transform here to 15-12-2024
     df = df.drop('date',axis=1)
     mask = df['temperature'] == '-'
     # select all rows except the ones masked out
